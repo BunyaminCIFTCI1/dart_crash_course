9c5041d9cffc1aa2775b0e3d094e1f560a29e560
BunyaminCIFTCI1 <bunyaminciftci1249@gmail.com>
2023-09-04 00:51:48 +0300

    operators

---
+++ operators/bin/example1.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: unused_local_variable
+
+void main(List<String> args) {
+  // ~/ is truncating divison operator
+  // result of division is always double value
+  // --num decreases value by one at the same line and returns it
+}
---
+++ operators/bin/example2.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: unused_local_variable
+void main(List<String> args) {
+  // double and integers values can not be assigned to each other
+  // const number1 = 23;
+  // const double number2 = number1;
+  int age = 2333;
+  print(age = 23); // called as compound assignment operator
+  print(age += age);
+}
